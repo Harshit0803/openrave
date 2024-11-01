c6c17cbc4d57834a298e25fe666f30a36ba528e6
Shunichi Nozawa <shunichi.nozawa@mujin.co.jp>
2024-11-01 13:34:03 +0900

    Print _vNonAdjacentLinks when it's lazily computed, for better debugging.

--- src/libopenrave/robot.cpp
+++ src/libopenrave/robot.cpp
@@ -2091,6 +2091,7 @@ const std::vector<int>& RobotBase::GetNonAdjacentLinks(int adjacentoptions) cons
                 }
             }
             std::sort(_vNonAdjacentLinks[AO_Enabled].begin(), _vNonAdjacentLinks[AO_Enabled].end(), CompareNonAdjacentFarthest);
+            KinBody::_PrintNonAdjacentLinks(_vNonAdjacentLinks, AO_Enabled, GetEnv()->GetNameId(), GetName());
         }
         if( compute.at(AO_ActiveDOFs) ) {
             _vNonAdjacentLinks.at(AO_ActiveDOFs).resize(0);
@@ -2103,6 +2104,7 @@ const std::vector<int>& RobotBase::GetNonAdjacentLinks(int adjacentoptions) cons
                 }
             }
             std::sort(_vNonAdjacentLinks[AO_ActiveDOFs].begin(), _vNonAdjacentLinks[AO_ActiveDOFs].end(), CompareNonAdjacentFarthest);
+            KinBody::_PrintNonAdjacentLinks(_vNonAdjacentLinks, AO_ActiveDOFs, GetEnv()->GetNameId(), GetName());
         }
         if( compute.at(AO_Enabled|AO_ActiveDOFs) ) {
             _vNonAdjacentLinks.at(AO_Enabled|AO_ActiveDOFs).resize(0);
@@ -2113,6 +2115,7 @@ const std::vector<int>& RobotBase::GetNonAdjacentLinks(int adjacentoptions) cons
                 }
             }
             std::sort(_vNonAdjacentLinks[AO_Enabled|AO_ActiveDOFs].begin(), _vNonAdjacentLinks[AO_Enabled|AO_ActiveDOFs].end(), CompareNonAdjacentFarthest);
+            KinBody::_PrintNonAdjacentLinks(_vNonAdjacentLinks, AO_Enabled|AO_ActiveDOFs, GetEnv()->GetNameId(), GetName());
         }
         _nNonAdjacentLinkCache |= requestedoptions;
     }
